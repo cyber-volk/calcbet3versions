abc5d51822ba05fa7e69ff636783048303a28b46
cyber-volk <mastervolk160@gmail.com>
2024-11-10 23:11:37 +0100

    Fix 1234

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig 
